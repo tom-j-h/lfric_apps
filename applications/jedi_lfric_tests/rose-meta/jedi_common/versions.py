5bc263f0740159da075bc95dce1488699a90ca71
tom-j-h <tom.hill@metoffice.gov.uk>
2026-01-29 19:28:58 +0000

    apply_macros

--- applications/jedi_lfric_tests/rose-meta/jedi_common/versions.py
+++ applications/jedi_lfric_tests/rose-meta/jedi_common/versions.py
@@ -20,13 +20,10 @@ class UpgradeError(Exception):
 
 """
 Copy this template and complete to add your macro
-
 class vnXX_txxx(MacroUpgrade):
     # Upgrade macro for <TICKET> by <Author>
-
     BEFORE_TAG = "vnX.X"
     AFTER_TAG = "vnX.X_txxx"
-
     def upgrade(self, config, meta_config=None):
         # Add settings
         return config, self.reports
@@ -47,11 +44,69 @@ class vn30_t99(MacroUpgrade):
 
 
 class vn30_t132(MacroUpgrade):
-    # Upgrade macro for #132 by Tom Hill
+    """Upgrade macro for ticket #132 by Tom Hill."""
 
     BEFORE_TAG = "vn3.0_t99"
     AFTER_TAG = "vn3.0_t132"
 
     def upgrade(self, config, meta_config=None):
-        self.add_setting(config, ["namelist:jedi_lfric_settings", "adjoint_test_tolerance"], "1.0e-4")
+        # Commands From: rose-meta/jedi_common
+        self.add_setting(
+            config,
+            ["namelist:jedi_lfric_settings", "adjoint_test_tolerance"],
+            "1.0e-4",
+        )
+
+        return config, self.reports
+
+
+class vn30_t182(MacroUpgrade):
+    """Upgrade macro for ticket #182 by Tom Hill."""
+
+    BEFORE_TAG = "vn3.0_t132"
+    AFTER_TAG = "vn3.0_t182"
+
+    def upgrade(self, config, meta_config=None):
+        # Commands From: rose-meta/lfric-linear
+        """Add linear boundary layer physics scheme"""
+        scaling = self.get_setting_value(
+            config, ["namelist:planet", "scaling_factor"]
+        )
+        if "125.0" in scaling:
+            self.add_setting(
+                config,
+                ["namelist:linear_physics", "l_boundary_layer"],
+                ".false.",
+            )
+        else:
+            self.add_setting(
+                config,
+                ["namelist:linear_physics", "l_boundary_layer"],
+                ".true.",
+            )
+            self.add_setting(
+                config, ["namelist:linear_physics", "Blevs_m"], "15"
+            )
+            self.add_setting(
+                config, ["namelist:linear_physics", "e_folding_levs_m"], "10"
+            )
+            self.add_setting(
+                config, ["namelist:linear_physics", "l_0_m"], "80.0"
+            )
+            self.add_setting(
+                config, ["namelist:linear_physics", "log_layer"], "2"
+            )
+            self.add_setting(
+                config, ["namelist:linear_physics", "u_land_m"], "0.4"
+            )
+            self.add_setting(
+                config, ["namelist:linear_physics", "u_sea_m"], "0.4"
+            )
+            self.add_setting(
+                config, ["namelist:linear_physics", "z_land_m"], "0.05"
+            )
+            self.add_setting(
+                config, ["namelist:linear_physics", "z_sea_m"], "0.0005"
+            )
+
         return config, self.reports
--- applications/jedi_lfric_tests/rose-meta/jedi_forecast_pseudo/versions.py
+++ applications/jedi_lfric_tests/rose-meta/jedi_forecast_pseudo/versions.py
@@ -45,3 +45,72 @@ class vn30_t99(MacroUpgrade):
         self.add_setting(config, ["namelist:physics", "conv_gr_segment"], "16")
 
         return config, self.reports
+
+
+class vn30_t132(MacroUpgrade):
+    """Upgrade macro for ticket #132 by Tom Hill."""
+
+    BEFORE_TAG = "vn3.0_t99"
+    AFTER_TAG = "vn3.0_t132"
+
+    def upgrade(self, config, meta_config=None):
+        # Commands From: rose-meta/jedi_common
+        self.add_setting(
+            config,
+            ["namelist:jedi_lfric_settings", "adjoint_test_tolerance"],
+            "1.0e-4",
+        )
+
+        return config, self.reports
+
+
+class vn30_t182(MacroUpgrade):
+    """Upgrade macro for ticket #182 by Tom Hill."""
+
+    BEFORE_TAG = "vn3.0_t132"
+    AFTER_TAG = "vn3.0_t182"
+
+    def upgrade(self, config, meta_config=None):
+        # Commands From: rose-meta/lfric-linear
+        """Add linear boundary layer physics scheme"""
+        scaling = self.get_setting_value(
+            config, ["namelist:planet", "scaling_factor"]
+        )
+        if "125.0" in scaling:
+            self.add_setting(
+                config,
+                ["namelist:linear_physics", "l_boundary_layer"],
+                ".false.",
+            )
+        else:
+            self.add_setting(
+                config,
+                ["namelist:linear_physics", "l_boundary_layer"],
+                ".true.",
+            )
+            self.add_setting(
+                config, ["namelist:linear_physics", "Blevs_m"], "15"
+            )
+            self.add_setting(
+                config, ["namelist:linear_physics", "e_folding_levs_m"], "10"
+            )
+            self.add_setting(
+                config, ["namelist:linear_physics", "l_0_m"], "80.0"
+            )
+            self.add_setting(
+                config, ["namelist:linear_physics", "log_layer"], "2"
+            )
+            self.add_setting(
+                config, ["namelist:linear_physics", "u_land_m"], "0.4"
+            )
+            self.add_setting(
+                config, ["namelist:linear_physics", "u_sea_m"], "0.4"
+            )
+            self.add_setting(
+                config, ["namelist:linear_physics", "z_land_m"], "0.05"
+            )
+            self.add_setting(
+                config, ["namelist:linear_physics", "z_sea_m"], "0.0005"
+            )
+
+        return config, self.reports
